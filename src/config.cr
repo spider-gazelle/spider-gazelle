9544b65626ac2084d6359eaf1e52d7349adfec00
Stephen von Takach <steve@cotag.me>
2018-03-18 11:46:53 +1100

    (config) add compress handler
    also have static file handler run before the application

--- src/config.cr
+++ src/config.cr
@@ -10,23 +10,24 @@ require "./models/*"
 # Server required after application controllers
 require "action-controller/server"
 
-# Optional support for serving of static assests
-static_file_path = ENV["PUBLIC_WWW_PATH"]? || "./www"
-if File.directory?(static_file_path)
-  # Add additional mime types
-  ActionController::FileHandler::MIME_TYPES[".yaml"] = "text/yaml"
-
-  # Check for files if no paths matched in your application
-  ActionController::Server.after(
-    ActionController::FileHandler.new(static_file_path)
-  )
-end
-
 # Add handlers that should run before your application
 ActionController::Server.before(
   HTTP::LogHandler.new(STDOUT),
-  HTTP::ErrorHandler.new(ENV["SG_ENV"]? != "production")
+  HTTP::ErrorHandler.new(ENV["SG_ENV"]? != "production"),
+  HTTP::CompressHandler.new
 )
+
+# Optional support for serving of static assests
+static_file_path = ENV["PUBLIC_WWW_PATH"]? || "./www"
+if File.directory?(static_file_path)
+  # Optionally add additional mime types
+  ActionController::FileHandler::MIME_TYPES[".yaml"] = "text/yaml"
+
+  # Check for files if no paths matched in your application
+  ActionController::Server.before(
+    ActionController::FileHandler.new(static_file_path, directory_listing: false)
+  )
+end
 
 # Configure session cookies
 # NOTE:: Change these from defaults
