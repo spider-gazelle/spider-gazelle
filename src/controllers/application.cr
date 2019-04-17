b6c9918462ef508079b8b7f10c4e6c05faced12b
Stephen von Takach <steve@acaprojects.com>
2019-04-17 15:02:46 +1000

    add tagged logging support
    Makes it simple to trace requests through micro-services whilst leaving the developer in complete control of individual implementations.
    
    For example public APIs generate random request IDs and then passes these IDs to private APIs via a request header, allowing for tracability.

--- src/controllers/application.cr
+++ src/controllers/application.cr
@@ -1,5 +1,12 @@
+require "uuid"
+
 abstract class Application < ActionController::Base
+  before_action :set_request_id
   before_action :set_date_header
+
+  def set_request_id
+    response.headers["X-Request-ID"] = request.id = UUID.random.to_s
+  end
 
   def set_date_header
     response.headers["Date"] = HTTP.format_time(Time.now)
