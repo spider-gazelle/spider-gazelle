a5035a25504a84c849f65d909ee9036e72e85794
Stephen von Takach <steve@advancedcontrol.com.au>
2019-05-20 11:54:50 +1000

    (controllers:application) note about request IDs

--- src/controllers/application.cr
+++ src/controllers/application.cr
@@ -4,8 +4,13 @@ abstract class Application < ActionController::Base
   before_action :set_request_id
   before_action :set_date_header
 
+  # This makes it simple to match client requests with server side logs.
+  # When building microservices this ID should be propagated to upstream services.
   def set_request_id
     response.headers["X-Request-ID"] = request.id = UUID.random.to_s
+
+    # If this is an upstream service, the ID should be extracted from a request header.
+    # response.headers["X-Request-ID"] = request.id = request.headers["X-Request-ID"]? || UUID.random.to_s
   end
 
   def set_date_header
